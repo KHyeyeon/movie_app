ba94b70a8f9ca60d1cb37725dcfea854a033ee6b
Hyeyeon <gpdus982@gmail.com>
2020-07-15 00:39:46 +0900

    Updates

---
+++ precache-manifest.cbe6d24d8b5255258b9deb8180e757cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6390cf2f3193d3b4424940a25b42a7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d3e69198de5954d5778f",
+    "url": "/movie_app/static/css/main.13676ee6.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e69198de5954d5778f",
+    "url": "/movie_app/static/js/main.8160cff6.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
